8ad824d5dee96e6f898da2d69b435ee959f9cef1
lyn0531 <47732410+lyn0531@users.noreply.github.com>
2019-02-18 16:27:48 +0900

    Add files via upload

---
+++ cds_led_serial.ino
@@ -0,0 +1,22 @@
+int led = 3;
+int cds = A0;
+
+void setup() {
+  Serial.begin(9600); //추가
+  pinMode(led, OUTPUT); 
+}
+
+void loop() {
+  int light = analogRead(cds); 
+  Serial.println(light); //추가
+  delay(500); //추가
+
+  if (light < 600) {
+    digitalWrite(led, HIGH);
+  }
+  
+  else {
+    digitalWrite(led, LOW);
+  }
+
+}
